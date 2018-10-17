22234f20632bee6bf5230add865df5b97ec08e37
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-10-17 12:37:26 +0200

    Get photo ids from album

--- facebook_photos.go
+++ facebook_photos.go
@@ -4,26 +4,43 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
+	"os/user"
 
 	fb "github.com/huandu/facebook"
 )
-
-type User struct {
-	FirstName string
-}
 
 var USERNAME = flag.String("username", "", "Facebook username")
 var TOKEN = flag.String("token", "", "Facebook token")
 
 func runFacebookQuery(query string) (result fb.Result) {
+	fmt.Println(query)
 	res, err := fb.Get(query, fb.Params{
-		"fields": "first_name",
 		"access_token": *TOKEN,
 	})
 	if err != nil {
 		log.Fatalln("Could not access your feed")
 	}
 	return res
+}
+
+func downloadPhoto(query string, baseDir string) {
+	// Save photo to disk
+}
+
+func downloadAlbumPhotos(name string, albumId string, baseDir string) {
+	dir := fmt.Sprintf("%v/%v/%v", baseDir, name, albumId)
+	os.MkdirAll(dir, 0755)
+
+	albumQuery := "/" + albumId + "/photos"
+	albumResult := runFacebookQuery(albumQuery)
+	var photos []fb.Result
+	albumResult.DecodeField("data", &photos)
+
+	for _, photo := range photos {
+		photoId := photo["id"].(string)
+		downloadPhoto(photoId, dir)
+	}
 }
 
 func main() {
@@ -33,18 +50,23 @@ func main() {
 		log.Fatalln("You need to provide a token (--token=<token>)")
 	}
 
-	userQuery := "/me"
+	usr, _ := user.Current()
+	baseDir := fmt.Sprintf("%v/Pictures/Facebook", usr.HomeDir)
+	fmt.Println("Saving files to", baseDir)
+
+	userQuery := "/me?fields=first_name"
 	userResult := runFacebookQuery(userQuery)
 	var user User
 	userResult.Decode(&user)
 	fmt.Println("Downloading pictures for", user.FirstName)
 
-	albumQuery := userQuery + "/albums"
-	albumResult := runFacebookQuery(albumQuery)
+	albumsQuery := "/me/albums"
+	albumsResult := runFacebookQuery(albumsQuery)
 	var albums []fb.Result
-	albumResult.DecodeField("data", &albums)
-	fmt.Println("Album ids:")
+	albumsResult.DecodeField("data", &albums)
+
 	for _, album := range albums {
-		fmt.Println(album["id"])
+		albumId := album["id"].(string)
+		downloadAlbumPhotos(user.FirstName, albumId, baseDir)
 	}
 }
